c0dd1e5138d8db1061d57552ddfe86f565242ea8
GhaneshChoubey <anuchomah@gmail.com>
2024-02-25 19:11:54 +0530

    First commit

---
+++ center_name_print.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    gotoxy(60,13);
+    printf("Ghanesh Choubey");
+    return 0;
+}
---
+++ simple_print.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=3,b=51;
+  printf("The sum of %d and %d is %d",a,b,a+b);
+  getch();
+}
