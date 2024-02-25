6932b9f3f208d59ce115d51df50afce6f307995d
GhaneshChoubey <anuchomah@gmail.com>
2024-02-25 17:44:36 +0530

    First commit

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a,b,c;
+    printf("Enter Two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of %d and %d is %d",a,b,c);
+    getch();
+}
