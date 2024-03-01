8d65e25134e86902da328ffcc7356bf576e7d043
GhaneshChoubey <anuchomah@gmail.com>
2024-03-01 23:45:11 +0530

    first commit

---
+++ given_no_postive_or_not.c
@@ -0,0 +1,18 @@
+/*To Check Given number is positive or not*/
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a;
+    printf("Please enter two number:-");
+    scanf("%d",&a);
+
+    if(a>0)
+
+    printf("Enter number is Positive");
+
+    else
+
+    printf("Enter number is negative");
+}
